b1e057d392adf59cf86d00055f4117227c61b0f0
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-11-17 22:27:34 -0800

    Update LibLLVMSharp.ValueGetDemangledName

--- AssetRipper.Translation.Cpp/LibLLVMSharp.cs
+++ AssetRipper.Translation.Cpp/LibLLVMSharp.cs
@@ -1,10 +1,12 @@
 ﻿using LLVMSharp.Interop;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AssetRipper.Translation.Cpp;
 
 internal static unsafe class LibLLVMSharp
 {
+#pragma warning disable SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
 	[DllImport("libLLVMSharp", CallingConvention = CallingConvention.Cdecl, EntryPoint = "llvmsharp_Function_getReturnType", ExactSpelling = true)]
 	private static extern LLVMOpaqueType* FunctionGetReturnType(LLVMOpaqueValue* Fn);
 
@@ -12,7 +14,16 @@ internal static unsafe class LibLLVMSharp
 	private static extern LLVMOpaqueType* FunctionGetFunctionType(LLVMOpaqueValue* Fn);
 
 	[DllImport("libLLVMSharp", CallingConvention = CallingConvention.Cdecl, EntryPoint = "llvmsharp_Value_getDemangledName", ExactSpelling = true)]
-	private static extern char* ValueGetDemangledName(LLVMOpaqueValue* value);
+	private static extern int ValueGetDemangledName(LLVMOpaqueValue* value, byte* buffer, int buffer_size);
+#pragma warning restore SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+
+	private static int ValueGetDemangledName(LLVMOpaqueValue* value, Span<byte> buffer)
+	{
+		fixed (byte* bufferPtr = buffer)
+		{
+			return ValueGetDemangledName(value, bufferPtr, buffer.Length);
+		}
+	}
 
 	public static LLVMTypeRef FunctionGetReturnType(LLVMValueRef fn)
 	{
@@ -26,6 +37,13 @@ internal static unsafe class LibLLVMSharp
 
 	public static string? ValueGetDemangledName(LLVMValueRef value)
 	{
-		return Marshal.PtrToStringAnsi((IntPtr)ValueGetDemangledName((LLVMOpaqueValue*)value));
+		const int MaxLength = 4096;
+		Span<byte> buffer = stackalloc byte[MaxLength];
+		int length = ValueGetDemangledName((LLVMOpaqueValue*)value, buffer);
+		if (length == 0)
+		{
+			return null;
+		}
+		return Encoding.UTF8.GetString(buffer[..length]);
 	}
 }
