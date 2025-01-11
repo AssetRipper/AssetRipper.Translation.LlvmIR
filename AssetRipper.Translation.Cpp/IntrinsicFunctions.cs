513b389980a00eecdefa57587675fc75991f1677
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-01-11 01:35:21 -0800

    Hide warnings for unused intrinsic function parameters

--- AssetRipper.Translation.Cpp/IntrinsicFunctions.cs
+++ AssetRipper.Translation.Cpp/IntrinsicFunctions.cs
@@ -2,6 +2,7 @@
 
 namespace AssetRipper.Translation.Cpp;
 
+#pragma warning disable IDE0060 // Remove unused parameter
 internal static class IntrinsicFunctions
 {
 	public unsafe static void llvm_memcpy_p0_p0_i32(void* destination, void* source, int length, bool isVolatile)
@@ -14,3 +15,4 @@ internal static class IntrinsicFunctions
 		Unsafe.CopyBlock(destination, source, (uint)length);
 	}
 }
+#pragma warning restore IDE0060 // Remove unused parameter
