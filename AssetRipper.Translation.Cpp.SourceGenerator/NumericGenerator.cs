51412987b0f5b3ce7aa31bb66ca14ba11585865a
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-06-12 20:35:24 -0700

    Source generate simple operations

--- AssetRipper.Translation.Cpp.SourceGenerator/NumericGenerator.cs
+++ AssetRipper.Translation.Cpp.SourceGenerator/NumericGenerator.cs
@@ -14,9 +14,19 @@ public partial class NumericGenerator() : IncrementalGenerator(nameof(NumericGen
 		("int", "uint"),
 		("nint", "nuint"),
 		("long", "ulong"),
+		("Int128", "UInt128"),
 	];
 
 	private static IEnumerable<string> IntegerTypes => IntegerTypePairs.SelectMany(pair => (IEnumerable<string>)[pair.Signed, pair.Unsigned]);
+
+	private static IEnumerable<(string Operation, char Symbol, string RequiredInterface)> SimpleOperations =>
+	[
+		("Add", '+', "IAdditionOperators<T, T, T>"),
+		("Subtract", '-', "ISubtractionOperators<T, T, T>"),
+		("Multiply", '*', "IMultiplyOperators<T, T, T>"),
+		("Divide", '/', "IDivisionOperators<T, T, T>"),
+		("Remainder", '%', "IModulusOperators<T, T, T>"),
+	];
 
 	public override void OnInitialize(SgfInitializationContext context)
 	{
@@ -33,6 +43,7 @@ public partial class NumericGenerator() : IncrementalGenerator(nameof(NumericGen
 
 		writer.WriteGeneratedCodeWarning();
 		writer.WriteLineNoTabs();
+		writer.WriteUsing("System.Numerics");
 		writer.WriteUsing("System.Runtime.CompilerServices");
 		writer.WriteLineNoTabs();
 		writer.WriteFileScopedNamespace("AssetRipper.Translation.Cpp");
@@ -55,6 +66,48 @@ public partial class NumericGenerator() : IncrementalGenerator(nameof(NumericGen
 				using (new Else(writer))
 				{
 					writer.WriteLine("return default;");
+				}
+			}
+			foreach ((string operation, char symbol, string requiredInterface) in SimpleOperations)
+			{
+				writer.WriteLineNoTabs();
+				writer.WriteLine($"[MethodImpl(MethodImplOptions.AggressiveInlining)]");
+				writer.WriteLine($"public static T {operation}<T>(T x, T y) where T : {requiredInterface}");
+				using (new CurlyBrackets(writer))
+				{
+					writer.WriteLine($"return unchecked(x {symbol} y);");
+				}
+				foreach (bool emittingSignedMethod in (ReadOnlySpan<bool>)[true, false])
+				{
+					writer.WriteLineNoTabs();
+					writer.WriteLine("[MethodImpl(MethodImplOptions.AggressiveInlining)]");
+					string methodName = emittingSignedMethod ? $"{operation}Signed" : $"{operation}Unsigned";
+					writer.WriteLine($"public static T {methodName}<T>(T x, T y) where T : {requiredInterface}");
+					using (new CurlyBrackets(writer))
+					{
+						using (new Checked(writer))
+						{
+							foreach ((string signedType, string unsignedType) in IntegerTypePairs)
+							{
+								(string operationType, string parameterType) = emittingSignedMethod
+									? (signedType, unsignedType)
+									: (unsignedType, signedType);
+								writer.WriteLine($"if (typeof(T) == typeof({parameterType}))");
+								using (new CurlyBrackets(writer))
+								{
+									writer.WriteLine($"{parameterType} x1 = ({parameterType})(object)x;");
+									writer.WriteLine($"{parameterType} y1 = ({parameterType})(object)y;");
+									writer.WriteLine($"{operationType} result1 = ({operationType})(unchecked(({operationType})x1) {symbol} unchecked(({operationType})y1));");
+									writer.WriteLine($"{parameterType} result2 = unchecked(({parameterType})result1);");
+									writer.WriteLine("return (T)(object)result2;");
+								}
+							}
+							using (new Else(writer))
+							{
+								writer.WriteLine($"return x {symbol} y;");
+							}
+						}
+					}
 				}
 			}
 		}
