ec91675be4a70912a37b809bebb7a038c4316e17
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-07-25 02:45:13 -0700

    Delete TranslationTests * Justification: too brittle

--- AssetRipper.Translation.LlvmIR.Tests/Extensions.cs
+++ AssetRipper.Translation.LlvmIR.Tests/Extensions.cs
@@ -5,16 +5,6 @@ namespace AssetRipper.Translation.LlvmIR.Tests;
 
 internal static class Extensions
 {
-	public static TypeDefinition GetGlobalFunctionsType(this ModuleDefinition module)
-	{
-		return module.TopLevelTypes.Single(t => t.Namespace is null && t.Name == "GlobalFunctions");
-	}
-
-	public static TypeDefinition GetPointerCacheType(this ModuleDefinition module)
-	{
-		return module.TopLevelTypes.Single(t => t.Namespace is null && t.Name == "PointerCache");
-	}
-
 	public static ModuleDefinition TranslateToCIL(this string text, [CallerMemberName] string? caller = null)
 	{
 		string name = string.IsNullOrEmpty(caller) ? nameof(TranslateToCIL) : caller;
