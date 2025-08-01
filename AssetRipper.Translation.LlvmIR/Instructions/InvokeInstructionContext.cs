6c02aa2e163ec284c59a3dcb75ee6bc9c5d2b1bf
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-08-01 01:56:10 -0700

    Remove an unnecessary branch when emitting for invoke instructions

--- AssetRipper.Translation.LlvmIR/Instructions/InvokeInstructionContext.cs
+++ AssetRipper.Translation.LlvmIR/Instructions/InvokeInstructionContext.cs
@@ -29,21 +29,36 @@ internal sealed class InvokeInstructionContext : BaseCallInstructionContext
 
 		base.AddInstructions(instructions);
 
-		CilInstructionLabel defaultLabel = new();
-
 		// if exception info is not null, branch to the catch block
+		// else branch to the default block
+
+		instructions.Add(CilOpCodes.Ldsfld, Module.InjectedTypes[typeof(ExceptionInfo)].GetFieldByName(nameof(ExceptionInfo.Current)));
+
+		if (!TargetBlockStartsWithPhi(CatchBlock))
 		{
-			instructions.Add(CilOpCodes.Ldsfld, Module.InjectedTypes[typeof(ExceptionInfo)].GetFieldByName(nameof(ExceptionInfo.Current)));
+			instructions.Add(CilOpCodes.Brtrue, CatchBlock.Label);
+
+			AddLoadIfBranchingToPhi(instructions, DefaultBlock);
+			instructions.Add(CilOpCodes.Br, DefaultBlock.Label);
+		}
+		else if (!TargetBlockStartsWithPhi(DefaultBlock))
+		{
+			instructions.Add(CilOpCodes.Brfalse, DefaultBlock.Label);
+
+			AddLoadIfBranchingToPhi(instructions, CatchBlock);
+			instructions.Add(CilOpCodes.Br, CatchBlock.Label);
+		}
+		else
+		{
+			CilInstructionLabel defaultLabel = new();
+
 			instructions.Add(CilOpCodes.Brfalse, defaultLabel);
 			AddLoadIfBranchingToPhi(instructions, CatchBlock);
-			instructions.Add(CilOpCodes.Br, Function.BasicBlockLookup[CatchBlockRef].Label);
-		}
+			instructions.Add(CilOpCodes.Br, CatchBlock.Label);
 
-		// else branch to the default block
-		{
 			int currentIndex = instructions.Count;
 			AddLoadIfBranchingToPhi(instructions, DefaultBlock);
-			instructions.Add(CilOpCodes.Br, Function.BasicBlockLookup[DefaultBlockRef].Label);
+			instructions.Add(CilOpCodes.Br, DefaultBlock.Label);
 			defaultLabel.Instruction = instructions[currentIndex];
 		}
 	}
