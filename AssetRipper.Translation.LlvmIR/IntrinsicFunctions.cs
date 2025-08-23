d2fb522cb0e49a92ee3e686bc99dfdaf141542e2
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-08-23 10:30:38 -0700

    Add missing using

--- AssetRipper.Translation.LlvmIR/IntrinsicFunctions.cs
+++ AssetRipper.Translation.LlvmIR/IntrinsicFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System.Buffers;
+﻿using AssetRipper.Translation.LlvmIR.Attributes;
+using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
