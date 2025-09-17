a391e590f349ce2f477131809c5fdd3eef051a6d
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-09-16 22:09:55 -0700

    LibLLVMSharp API expansion
    Related: #30

---
+++ AssetRipper.Translation.LlvmIR/LLVMMetadataRef.cs
@@ -0,0 +1,203 @@
+﻿using AssetRipper.Translation.LlvmIR.Extensions;
+using LLVMSharp.Interop;
+
+namespace AssetRipper.Translation.LlvmIR;
+
+public unsafe partial struct LLVMMetadataRef(IntPtr handle) : IEquatable<LLVMMetadataRef>
+{
+	public IntPtr Handle = handle;
+
+	public static implicit operator LLVMMetadataRef(LLVMOpaqueMetadata* value) => new LLVMMetadataRef((IntPtr)value);
+
+	public static implicit operator LLVMOpaqueMetadata*(LLVMMetadataRef value) => (LLVMOpaqueMetadata*)value.Handle;
+
+	public static bool operator ==(LLVMMetadataRef left, LLVMMetadataRef right) => left.Handle == right.Handle;
+
+	public static bool operator !=(LLVMMetadataRef left, LLVMMetadataRef right) => !(left == right);
+
+	public override readonly bool Equals(object? obj) => (obj is LLVMMetadataRef other) && Equals(other);
+
+	public readonly bool Equals(LLVMMetadataRef other) => this == other;
+
+	public override readonly int GetHashCode() => Handle.GetHashCode();
+
+	public override readonly string ToString() => Handle == default ? "null" : nameof(LLVMMetadataRef);
+
+	public readonly LLVMMetadataKind Kind => Handle == default
+		? (LLVMMetadataKind)(-1)
+		: (LLVMMetadataKind)LLVM.GetMetadataKind(this);
+
+	public readonly bool IsType => Kind switch
+	{
+		LLVMMetadataKind.LLVMDICompositeTypeMetadataKind => true,
+		LLVMMetadataKind.LLVMDIDerivedTypeMetadataKind => true,
+		LLVMMetadataKind.LLVMDIStringTypeMetadataKind => true,
+		LLVMMetadataKind.LLVMDIBasicTypeMetadataKind => true,
+		LLVMMetadataKind.LLVMDISubroutineTypeMetadataKind => true,
+		_ => false,
+	};
+
+	public readonly bool IsTemplateParameter => Kind switch
+	{
+		LLVMMetadataKind.LLVMDITemplateTypeParameterMetadataKind => true,
+		LLVMMetadataKind.LLVMDITemplateValueParameterMetadataKind => true,
+		_ => false,
+	};
+
+	public readonly bool IsVariable => Kind switch
+	{
+		LLVMMetadataKind.LLVMDILocalVariableMetadataKind => true,
+		LLVMMetadataKind.LLVMDIGlobalVariableMetadataKind => true,
+		_ => false,
+	};
+
+	public readonly bool IsDINode => Kind is >= LLVMMetadataKind.LLVMDILocationMetadataKind and <= LLVMMetadataKind.LLVMDIAssignIDMetadataKind;
+
+	public readonly uint AlignInBits => IsType
+		? LLVM.DITypeGetAlignInBits(this)
+		: default;
+
+	public readonly LLVMMetadataRef BaseType => Kind switch
+	{
+		LLVMMetadataKind.LLVMDIDerivedTypeMetadataKind => LibLLVMSharp.DIDerivedTypeGetBaseType(this),
+		LLVMMetadataKind.LLVMDICompositeTypeMetadataKind => LibLLVMSharp.DICompositeTypeGetBaseType(this),
+		_ => default,
+	};
+
+	public readonly uint Column => Kind switch
+	{
+		LLVMMetadataKind.LLVMDILocationMetadataKind => LLVM.DILocationGetColumn(this),
+		_ => default,
+	};
+
+	public readonly LLVMMetadataRef[] Elements => Kind switch
+	{
+		LLVMMetadataKind.LLVMDICompositeTypeMetadataKind => LibLLVMSharp.DICompositeTypeGetElements(this),
+		_ => [],
+	};
+
+	public readonly uint Encoding => Kind switch
+	{
+		LLVMMetadataKind.LLVMDIDerivedTypeMetadataKind => LibLLVMSharp.DIDerivedTypeGetEncoding(this),
+		_ => default,
+	};
+
+	public readonly LLVMMetadataRef Expression => Kind switch
+	{
+		LLVMMetadataKind.LLVMDIGlobalVariableExpressionMetadataKind => LLVM.DIGlobalVariableExpressionGetExpression(this),
+		_ => default,
+	};
+
+	public readonly LLVMMetadataRef File => Kind switch
+	{
+		LLVMMetadataKind.LLVMDIFileMetadataKind => this,
+		LLVMMetadataKind.LLVMDISubprogramMetadataKind => LLVM.DIScopeGetFile(this),
+		_ when IsVariable => LLVM.DIVariableGetFile(this),
+		_ when IsType => LLVM.DIScopeGetFile(this),
+		_ => default,
+	};
+
+	public readonly LLVMDIFlags Flags => IsType
+		? LLVM.DITypeGetFlags(this)
+		: Kind is LLVMMetadataKind.LLVMDISubprogramMetadataKind
+			? LibLLVMSharp.DISubprogramGetFlags(this)
+			: default;
+
+	public readonly string Identifier => Kind switch
+	{
+		LLVMMetadataKind.LLVMDICompositeTypeMetadataKind => LibLLVMSharp.DICompositeTypeGetIdentifier(this) ?? "",
+		_ => "",
+	};
+
+	public readonly LLVMMetadataRef InlinedAt => Kind switch
+	{
+		LLVMMetadataKind.LLVMDILocationMetadataKind => LLVM.DILocationGetInlinedAt(this),
+		_ => default,
+	};
+
+	public readonly uint Line => Kind switch
+	{
+		LLVMMetadataKind.LLVMDISubprogramMetadataKind => LLVM.DISubprogramGetLine(this),
+		LLVMMetadataKind.LLVMDILocationMetadataKind => LLVM.DILocationGetLine(this),
+		_ when IsType => LLVM.DITypeGetLine(this),
+		_ when IsVariable => LLVM.DIVariableGetLine(this),
+		_ => default,
+	};
+
+	public readonly string Name
+	{
+		get
+		{
+			if (IsType)
+			{
+				return LibLLVMSharp.DITypeGetName(this) ?? "";
+			}
+			else if (IsVariable)
+			{
+				return LibLLVMSharp.DIVariableGetName(this) ?? "";
+			}
+			else if (Kind == LLVMMetadataKind.LLVMDISubprogramMetadataKind)
+			{
+				return LibLLVMSharp.DISubprogramGetName(this) ?? "";
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+
+	public readonly ulong OffsetInBits => IsType
+		? LLVM.DITypeGetOffsetInBits(this)
+		: default;
+
+	public readonly LLVMMetadataRef Scope => Kind switch
+	{
+		LLVMMetadataKind.LLVMDILocationMetadataKind => LLVM.DILocationGetScope(this),
+		_ when IsVariable => LLVM.DIVariableGetScope(this),
+		_ => default,
+	};
+
+	public readonly ulong SizeInBits => IsType
+		? LLVM.DITypeGetSizeInBits(this)
+		: default;
+
+	public readonly uint SPFlags => Kind is LLVMMetadataKind.LLVMDISubprogramMetadataKind
+		? LibLLVMSharp.DISubprogramGetSPFlags(this)
+		: default;
+
+	public readonly ushort Tag => IsDINode
+		? LLVM.GetDINodeTag(this)
+		: default;
+
+	public readonly string TagString => IsDINode
+		? LibLLVMSharp.DINodeGetTagString(this) ?? ""
+		: "";
+
+	public readonly LLVMMetadataRef Type => Kind switch
+	{
+		LLVMMetadataKind.LLVMDILocalVariableMetadataKind => LibLLVMSharp.DIVariableGetType(this),
+		LLVMMetadataKind.LLVMDIGlobalVariableMetadataKind => LibLLVMSharp.DIVariableGetType(this),
+		LLVMMetadataKind.LLVMDISubprogramMetadataKind => LibLLVMSharp.DISubprogramGetType(this),
+		_ when IsTemplateParameter => LibLLVMSharp.DITemplateParameterGetType(this),
+		_ => default,
+	};
+
+	public readonly LLVMMetadataRef[] TypeArray => Kind switch
+	{
+		LLVMMetadataKind.LLVMDISubroutineTypeMetadataKind => LibLLVMSharp.DISubroutineTypeGetTypeArray(this),
+		_ => [],
+	};
+
+	public readonly LLVMMetadataRef Value => Kind switch
+	{
+		LLVMMetadataKind.LLVMDITemplateValueParameterMetadataKind => LibLLVMSharp.DITemplateValueParameterGetValue(this),
+		_ => default,
+	};
+
+	public readonly LLVMMetadataRef Variable => Kind switch
+	{
+		LLVMMetadataKind.LLVMDIGlobalVariableExpressionMetadataKind => LLVM.DIGlobalVariableExpressionGetVariable(this),
+		_ => default,
+	};
+}
--- AssetRipper.Translation.LlvmIR/LibLLVMSharp.cs
+++ AssetRipper.Translation.LlvmIR/LibLLVMSharp.cs
@@ -7,31 +7,42 @@ namespace AssetRipper.Translation.LlvmIR;
 
 internal static unsafe partial class LibLLVMSharp
 {
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Free")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial void Free(void* obj);
+
 	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Function_getReturnType")]
 	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
 	private static partial LLVMOpaqueType* FunctionGetReturnType(LLVMOpaqueValue* Fn);
+
+	public static LLVMTypeRef FunctionGetReturnType(LLVMValueRef fn)
+	{
+		return FunctionGetReturnType((LLVMOpaqueValue*)fn);
+	}
 
 	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Function_getFunctionType")]
 	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
 	private static partial LLVMOpaqueType* FunctionGetFunctionType(LLVMOpaqueValue* Fn);
 
+	public static LLVMTypeRef FunctionGetFunctionType(LLVMValueRef fn)
+	{
+		return FunctionGetFunctionType((LLVMOpaqueValue*)fn);
+	}
+
 	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_ConstantDataArray_getData")]
 	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
 	private static partial byte* ConstantDataArrayGetData(LLVMOpaqueValue* ConstantDataArray, int* out_size);
 
+	public static ReadOnlySpan<byte> ConstantDataArrayGetData(LLVMValueRef constantDataArray)
+	{
+		int size;
+		byte* data = ConstantDataArrayGetData((LLVMOpaqueValue*)constantDataArray, &size);
+		return new ReadOnlySpan<byte>(data, size);
+	}
+
 	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Value_getDemangledName")]
 	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
 	private static partial int ValueGetDemangledName(LLVMOpaqueValue* value, byte* buffer, int buffer_size);
-
-	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Instruction_hasNoSignedWrap")]
-	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-	[return: MarshalAs(UnmanagedType.U1)]
-	private static partial bool InstructionHasNoSignedWrap(LLVMOpaqueValue* instruction);
-
-	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Instruction_hasNoUnsignedWrap")]
-	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-	[return: MarshalAs(UnmanagedType.U1)]
-	private static partial bool InstructionHasNoUnsignedWrap(LLVMOpaqueValue* instruction);
 
 	private static int ValueGetDemangledName(LLVMOpaqueValue* value, Span<byte> buffer)
 	{
@@ -39,23 +50,6 @@ internal static unsafe partial class LibLLVMSharp
 		{
 			return ValueGetDemangledName(value, bufferPtr, buffer.Length);
 		}
-	}
-
-	public static LLVMTypeRef FunctionGetReturnType(LLVMValueRef fn)
-	{
-		return FunctionGetReturnType((LLVMOpaqueValue*)fn);
-	}
-
-	public static LLVMTypeRef FunctionGetFunctionType(LLVMValueRef fn)
-	{
-		return FunctionGetFunctionType((LLVMOpaqueValue*)fn);
-	}
-
-	public static ReadOnlySpan<byte> ConstantDataArrayGetData(LLVMValueRef constantDataArray)
-	{
-		int size;
-		byte* data = ConstantDataArrayGetData((LLVMOpaqueValue*)constantDataArray, &size);
-		return new ReadOnlySpan<byte>(data, size);
 	}
 
 	public static string? ValueGetDemangledName(LLVMValueRef value)
@@ -70,13 +64,246 @@ internal static unsafe partial class LibLLVMSharp
 		return Encoding.UTF8.GetString(buffer[..length]);
 	}
 
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Instruction_hasNoSignedWrap")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	[return: MarshalAs(UnmanagedType.U1)]
+	private static partial bool InstructionHasNoSignedWrap(LLVMOpaqueValue* instruction);
+
 	public static bool InstructionHasNoSignedWrap(LLVMValueRef instruction)
 	{
 		return InstructionHasNoSignedWrap((LLVMOpaqueValue*)instruction);
 	}
 
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_Instruction_hasNoUnsignedWrap")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	[return: MarshalAs(UnmanagedType.U1)]
+	private static partial bool InstructionHasNoUnsignedWrap(LLVMOpaqueValue* instruction);
+
 	public static bool InstructionHasNoUnsignedWrap(LLVMValueRef instruction)
 	{
 		return InstructionHasNoUnsignedWrap((LLVMOpaqueValue*)instruction);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DICompositeType_getBaseType")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial LLVMOpaqueMetadata* DICompositeTypeGetBaseType(LLVMOpaqueMetadata* type);
+
+	public static LLVMMetadataRef DICompositeTypeGetBaseType(LLVMMetadataRef type)
+	{
+		return DICompositeTypeGetBaseType((LLVMOpaqueMetadata*)type);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DICompositeType_getElements")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial void DICompositeTypeGetElements(LLVMOpaqueMetadata* type, LLVMOpaqueMetadata*** out_buffer, int* out_size);
+
+	public static LLVMMetadataRef[] DICompositeTypeGetElements(LLVMMetadataRef type)
+	{
+		LLVMOpaqueMetadata** buffer = null;
+		int size = 0;
+		DICompositeTypeGetElements((LLVMOpaqueMetadata*)type, &buffer, &size);
+		if (buffer == null)
+		{
+			if (buffer != null)
+			{
+				Free(buffer);
+			}
+			return [];
+		}
+
+		if (size == 0)
+		{
+			Free(buffer);
+			return [];
+		}
+
+		LLVMMetadataRef[] result = new LLVMMetadataRef[size];
+		for (int i = 0; i < size; i++)
+		{
+			result[i] = (LLVMMetadataRef)buffer[i];
+		}
+
+		Free(buffer);
+
+		return result;
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DICompositeType_getIdentifier")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial byte* DICompositeTypeGetIdentifier(LLVMOpaqueMetadata* type, int* out_size);
+
+	public static string? DICompositeTypeGetIdentifier(LLVMMetadataRef type)
+	{
+		int size = 0;
+		byte* ptr = DICompositeTypeGetIdentifier((LLVMOpaqueMetadata*)type, &size);
+		if (ptr == null || size == 0)
+		{
+			return null;
+		}
+		return Encoding.UTF8.GetString(new ReadOnlySpan<byte>(ptr, size));
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DIDerivedType_getBaseType")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial LLVMOpaqueMetadata* DIDerivedTypeGetBaseType(LLVMOpaqueMetadata* type);
+
+	public static LLVMMetadataRef DIDerivedTypeGetBaseType(LLVMMetadataRef type)
+	{
+		return DIDerivedTypeGetBaseType((LLVMOpaqueMetadata*)type);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DIDerivedType_getEncoding")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial uint DIDerivedTypeGetEncoding(LLVMOpaqueMetadata* type);
+
+	public static uint DIDerivedTypeGetEncoding(LLVMMetadataRef type)
+	{
+		return DIDerivedTypeGetEncoding((LLVMOpaqueMetadata*)type);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DINode_getTagString")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial sbyte* DINodeGetTagString(LLVMOpaqueMetadata* node, nuint* out_size);
+
+	public static string? DINodeGetTagString(LLVMMetadataRef node)
+	{
+		nuint size = 0;
+		sbyte* ptr = DINodeGetTagString((LLVMOpaqueMetadata*)node, &size);
+		if (ptr == null || size == 0)
+		{
+			return null;
+		}
+		return Encoding.UTF8.GetString((byte*)ptr, (int)size);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DISubprogram_getType")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial LLVMOpaqueMetadata* DISubprogramGetType(LLVMOpaqueMetadata* subprogram);
+
+	public static LLVMMetadataRef DISubprogramGetType(LLVMMetadataRef subprogram)
+	{
+		return DISubprogramGetType((LLVMOpaqueMetadata*)subprogram);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DISubprogram_getFlags")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial uint DISubprogramGetFlags(LLVMOpaqueMetadata* subprogram);
+
+	public static LLVMDIFlags DISubprogramGetFlags(LLVMMetadataRef subprogram)
+	{
+		return (LLVMDIFlags)DISubprogramGetFlags((LLVMOpaqueMetadata*)subprogram);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DISubprogram_getName")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial byte* DISubprogramGetName(LLVMOpaqueMetadata* subprogram, int* out_size);
+
+	public static string? DISubprogramGetName(LLVMMetadataRef subprogram)
+	{
+		int size = 0;
+		byte* ptr = DISubprogramGetName((LLVMOpaqueMetadata*)subprogram, &size);
+		if (ptr == null || size == 0)
+		{
+			return null;
+		}
+		return Encoding.UTF8.GetString(new ReadOnlySpan<byte>(ptr, size));
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DISubprogram_getSPFlags")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial uint DISubprogramGetSPFlags(LLVMOpaqueMetadata* subprogram);
+
+	public static uint DISubprogramGetSPFlags(LLVMMetadataRef subprogram)
+	{
+		return DISubprogramGetSPFlags((LLVMOpaqueMetadata*)subprogram);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DISubroutineType_getTypeArray")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial void DISubroutineTypeGetTypeArray(LLVMOpaqueMetadata* subroutine_type, LLVMOpaqueMetadata*** out_buffer, int* out_size);
+
+	public static LLVMMetadataRef[] DISubroutineTypeGetTypeArray(LLVMMetadataRef subroutineType)
+	{
+		LLVMOpaqueMetadata** buffer = null;
+		int size = 0;
+		DISubroutineTypeGetTypeArray((LLVMOpaqueMetadata*)subroutineType, &buffer, &size);
+		if (buffer == null)
+		{
+			if (buffer != null)
+			{
+				Free(buffer);
+			}
+			return [];
+		}
+
+		if (size == 0)
+		{
+			Free(buffer);
+			return [];
+		}
+
+		LLVMMetadataRef[] result = new LLVMMetadataRef[size];
+		for (int i = 0; i < size; i++)
+		{
+			result[i] = (LLVMMetadataRef)buffer[i];
+		}
+
+		Free(buffer);
+
+		return result;
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DITemplateParameter_getType")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial LLVMOpaqueMetadata* DITemplateParameterGetType(LLVMOpaqueMetadata* parameter);
+
+	public static LLVMMetadataRef DITemplateParameterGetType(LLVMMetadataRef parameter)
+	{
+		return DITemplateParameterGetType((LLVMOpaqueMetadata*)parameter);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DITemplateValueParameter_getValue")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial LLVMOpaqueMetadata* DITemplateValueParameterGetValue(LLVMOpaqueMetadata* parameter);
+
+	public static LLVMMetadataRef DITemplateValueParameterGetValue(LLVMMetadataRef parameter)
+	{
+		return DITemplateValueParameterGetValue((LLVMOpaqueMetadata*)parameter);
+	}
+
+	public static string? DITypeGetName(LLVMMetadataRef type)
+	{
+		nuint nameLength = 0;
+		sbyte* namePtr = LLVM.DITypeGetName(type, &nameLength);
+		if (namePtr == null || nameLength == 0)
+		{
+			return null;
+		}
+
+		return Encoding.UTF8.GetString((byte*)namePtr, (int)nameLength);
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DIVariable_getName")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial byte* DIVariableGetName(LLVMOpaqueMetadata* variable, int* out_size);
+
+	public static string? DIVariableGetName(LLVMMetadataRef variable)
+	{
+		int size = 0;
+		byte* ptr = DIVariableGetName((LLVMOpaqueMetadata*)variable, &size);
+		if (ptr == null || size == 0)
+		{
+			return null;
+		}
+		return Encoding.UTF8.GetString(new ReadOnlySpan<byte>(ptr, size));
+	}
+
+	[LibraryImport("libLLVMSharp", EntryPoint = "llvmsharp_DIVariable_getType")]
+	[UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+	private static partial LLVMOpaqueMetadata* DIVariableGetType(LLVMOpaqueMetadata* variable);
+
+	public static LLVMMetadataRef DIVariableGetType(LLVMMetadataRef variable)
+	{
+		return DIVariableGetType((LLVMOpaqueMetadata*)variable);
 	}
 }
