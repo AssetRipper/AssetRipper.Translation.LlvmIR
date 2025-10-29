eb9e896eb7fac632046542ec25a3709c3dea6006
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-10-28 22:15:19 -0700

    Improve name generation
    * Use 6 characters in hash string instead of 7
    * Use xxHash32 instead of CRC32
    * Change the set of characters used in the hash string to be more visually distinct

--- AssetRipper.Translation.LlvmIR/NameGenerator.cs
+++ AssetRipper.Translation.LlvmIR/NameGenerator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Buffers;
-using System.Buffers.Binary;
 using System.Diagnostics.CodeAnalysis;
 using System.IO.Hashing;
 using System.Text;
@@ -12,12 +11,10 @@ internal static partial class NameGenerator
 	/// <summary>
 	/// Not official
 	/// </summary>
-	private const string Base32Characters = "abcdefghijklmnopqrstuvwxyz234579";
-
-	/// <summary>
-	/// Modified: replaced + and / with Σ (sigma) and Ω (omega).
-	/// </summary>
-	private const string Base64Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ΣΩ";
+	/// <remarks>
+	/// Characters chosen to avoid visually similar characters (l and 1, O and 0).
+	/// </remarks>
+	private const string Base32Characters = "abcdefghijkmnpqrstuvwxyz23456789";
 
 	public static bool IsValidCSharpName(string name)
 	{
@@ -45,42 +42,27 @@ internal static partial class NameGenerator
 	public static string GenerateName(string cleanName, string name, int index = 0)
 	{
 		uint hash = Hash(name, index);
-		Span<char> buffer = stackalloc char[7];
-		WriteBase32(hash, buffer);
+		Span<char> buffer =
+		[
+			Base32Characters[(int)(hash & 0x1F)],
+			Base32Characters[(int)((hash >> 5) & 0x1F)],
+			Base32Characters[(int)((hash >> 10) & 0x1F)],
+			Base32Characters[(int)((hash >> 15) & 0x1F)],
+			Base32Characters[(int)((hash >> 20) & 0x1F)],
+			Base32Characters[(int)((hash >> 25) & 0x1F)],
+		];
 		return $"{cleanName}_{buffer}";
 
-		static uint Hash(string str, int integer)
+		static uint Hash(string str, int index)
 		{
-			int length = Encoding.UTF8.GetByteCount(str) + sizeof(int);
+			int length = Encoding.UTF8.GetByteCount(str);
 			byte[] bytes = ArrayPool<byte>.Shared.Rent(length);
 			Span<byte> span = new(bytes, 0, length);
-			BinaryPrimitives.WriteInt32LittleEndian(span, integer);
-			Encoding.UTF8.GetBytes(str, span[sizeof(int)..]);
-			uint hash = Crc32.HashToUInt32(span);
+			Encoding.UTF8.GetBytes(str, span);
+			uint hash = XxHash32.HashToUInt32(span, index);
 			ArrayPool<byte>.Shared.Return(bytes);
 			return hash;
 		}
-	}
-
-	private static void WriteBase32(uint value, Span<char> buffer)
-	{
-		buffer[0] = Base32Characters[(int)(value & 0x1F)];
-		buffer[1] = Base32Characters[(int)((value >> 5) & 0x1F)];
-		buffer[2] = Base32Characters[(int)((value >> 10) & 0x1F)];
-		buffer[3] = Base32Characters[(int)((value >> 15) & 0x1F)];
-		buffer[4] = Base32Characters[(int)((value >> 20) & 0x1F)];
-		buffer[5] = Base32Characters[(int)((value >> 25) & 0x1F)];
-		buffer[6] = Base32Characters[(int)((value >> 30) & 0x1F)];
-	}
-
-	private static void WriteBase64(uint value, Span<char> buffer)
-	{
-		buffer[0] = Base64Characters[(int)(value & 0x3F)];
-		buffer[1] = Base64Characters[(int)((value >> 6) & 0x3F)];
-		buffer[2] = Base64Characters[(int)((value >> 12) & 0x3F)];
-		buffer[3] = Base64Characters[(int)((value >> 18) & 0x3F)];
-		buffer[4] = Base64Characters[(int)((value >> 24) & 0x3F)];
-		buffer[5] = Base64Characters[(int)((value >> 30) & 0x3F)];
 	}
 
 	[GeneratedRegex(@"\W")]
